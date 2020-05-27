0c29902c398c1b3647507b5c44c35f470ce4c8ab
Luwafemi A <aladefemi1@gmail.com>
2020-05-27 17:47:53 +0100

    Update precache-manifest.8b734a85b0f92afa28da59ac99b94a6a.js

--- precache-manifest.8b734a85b0f92afa28da59ac99b94a6a.js
+++ precache-manifest.8b734a85b0f92afa28da59ac99b94a6a.js
@@ -97,15 +97,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "dca0165c7b45e4e1d99839fd86c04993",
-    "url": "././static/media/talk-to-us.dca0165c.png"
+    "url": "./static/media/talk-to-us.dca0165c.png"
   },
   {
     "revision": "8fffc17f090c44691a90f8b9dcc418d4",
-    "url": "./static/media/video.8fffc17f.PNGG"
+    "url": "./static/media/video.8fffc17f.png"
   },
   {
     "revision": "bee8a2aa81b8a0f5b172f6c56d326f2e",
-    "url": "./static/media/videoAnimation.bee8a2aa.PNGG"
+    "url": "./static/media/videoAnimation.bee8a2aa.png"
   },
   {
     "revision": "327a202561f6de85b8aeaf7e5029cf63",
